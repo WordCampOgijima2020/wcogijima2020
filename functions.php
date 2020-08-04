214f2b7836e6f670ca79beec0eb466396af6d6e4
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2020-08-04 17:15:50 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 add_action( 'wp_enqueue_scripts', function() {
 	$time = get_theme_file_uri( '/src/css/style.css' );
+	wp_enqueue_style( 'twentytwenty-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'wcogijima2020', get_theme_file_uri( '/src/css/style.css' ), ['twentytwenty-style'], $time );
 } );
