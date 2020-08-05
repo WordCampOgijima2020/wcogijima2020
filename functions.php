fd1c2330b6130be3c80731554d8a35f6d88bb067
Toshiyuki Honda <rocket-martue@users.noreply.github.com>
2020-08-06 04:22:40 +0900

    Update functions.php
    開発環境の管理バーの色を変更

--- functions.php
+++ functions.php
@@ -4,3 +4,12 @@ add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'twentytwenty-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'wcogijima2020', get_theme_file_uri( '/src/css/style.css' ), ['twentytwenty-style'], $time );
 } );
+
+function mytheme_admin_bar() {
+	echo '<style type="text/css">
+	#wpadminbar {
+		background: #eb8f22;
+	}
+	</style>';
+}
+add_action( 'wp_head', 'mytheme_admin_bar' );
